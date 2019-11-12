e7bcff2bccb39f61981aa1fd42ba20c632c107a5
Alexander Ignatov <ani1802@abv.bg>
2019-11-12 23:21:25 +0200

    Remove unused imports

--- src/sample/XMLParserUtil.java
+++ src/sample/XMLParserUtil.java
@@ -3,14 +3,9 @@ package sample;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
 
