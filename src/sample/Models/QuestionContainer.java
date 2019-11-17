5c3550468370594e4541f24cd3e1506f95df7957
astoeff <36819826+astoeff@users.noreply.github.com>
2019-11-17 21:02:05 +0200

    Code review changes

--- src/sample/Models/QuestionContainer.java
+++ src/sample/Models/QuestionContainer.java
@@ -3,6 +3,25 @@ package sample.Models;
 import java.util.HashMap;
 
 public class QuestionContainer {
-    private HashMap<Answer, Integer> containerOfAllQuestions;
+    public HashMap<PriceCategory, Question> containerOfAllQuestions;
+    private static int numberOfAllQuestionsInContainer = 0;
+    private static final QuestionContainer instanceOfQuestionContainer = new QuestionContainer();
+
+    public void addQuestionToContainer(PriceCategory category, Question questionToAdd) {
+        containerOfAllQuestions.putIfAbsent(category, questionToAdd);
+        numberOfAllQuestionsInContainer++;
+    }
+
+    public int getNumberOfAllQuestionsInContainer() {
+        return numberOfAllQuestionsInContainer;
+    }
+
+    private QuestionContainer() {
+        containerOfAllQuestions = new HashMap<>();
+    }
+
+    public static QuestionContainer getInstanceOfQuestionContainer() {
+        return instanceOfQuestionContainer;
+    }
 
 }
